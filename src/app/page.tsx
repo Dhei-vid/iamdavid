f4f4dae5d2ed1b293c0fc64894fef50728deec39
David Etuk <djetuk@gmail.com>
2024-03-22 01:03:09 +0100

    added splash screen

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <main>
+      <Head>
+        <title>IamDavid</title>
+        <meta
+          name={"portfolio - davidXetuk"}
+          content={
+            "A portfolio talking about David and his work - Software Engineer"
+          }
+        />
+      </Head>
+
+      <body>
+        <h1>Home page</h1>
+      </body>
+    </main>
+  );
+};
+
+export default Home;
---
+++ src/app/tools/page.tsx
@@ -0,0 +1,9 @@
+const Tools = () => {
+  return (
+    <div>
+      <p className="text-xl font-bold">Tools I work with!</p>
+    </div>
+  );
+};
+
+export default Tools;
